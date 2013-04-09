4211acb7b52a4e71a1722348f60aac7b2ffd8c54
Rubin Siller <rubin.siller@rackspace.com>
2013-04-08 22:15:08 -0500

    4/8

---
+++ set_var.ps1
@@ -0,0 +1,10 @@
+﻿$user=""
+$APIkey=""
+$auth=""
+$region=""
+[Environment]::SetEnvironmentVariable("OS_USERNAME", "$user", "User")
+[Environment]::SetEnvironmentVariable("OS_TENANT_NAME", "$user", "User")
+[Environment]::SetEnvironmentVariable("OS_AUTH_SYSTEM", "rackspace", "User")
+[Environment]::SetEnvironmentVariable("OS_PASSWORD", "$APIkey", "User")
+[Environment]::SetEnvironmentVariable("OS_AUTH_URL", "$auth", "User")
+[Environment]::SetEnvironmentVariable("OS_REGION_NAME", "$region", "User")
